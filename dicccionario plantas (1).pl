d9f33808ae82422fb69b62a4996563b2ea44e880
Ruslan Dorantes Trejo <78804530+RuslanDT@users.noreply.github.com>
2022-11-29 11:03:38 -0600

    Add files via upload

---
+++ dicccionario plantas (1).pl
@@ -0,0 +1,219 @@
+planta(abrojo).
+planta(acacia).
+planta(acanto).
+planta(aceitilla).
+planta(achicoria).
+planta(briona_blanca).
+planta(canela).
+planta(cedron).
+planta(cardo_santo).
+planta(cempasuchil).
+
+
+
+cientifico(abrojo,'tribulus cistoides').
+cientifico(acacia,'').
+cientifico(acanto,'acantus mollis').
+cientifico(aceitilla,'Bidens leucantha').
+cientifico(achicoria,'chiconum incybus').
+cientifico(briona_blanca,'Bryomia dioca').
+cientifico(canela,'Laurus ciriamomum').
+cientifico(cedron,'lippia citriodora').
+cientifico(cardo_santo,'cirsium mexicanum').
+cientifico(cempasuchil,'tagetes erecta').
+
+
+
+como_Usar_Plantas(cocimiento,'Hervir agua, poner la planta, hervir 10 min, reposar 5 min').
+como_Usar_Plantas(infusion, 'Hervir agua, colocar la planta, tapar y reposar 5 min').
+como_Usar_Plantas(maceracion,'Triturar la planta, poner en agua y dejar remojar').
+como_Usar_Plantas(jarabe,'hervir 10 min, agregar azucar, agregar 10% de alcohol de caña y envasar').
+como_Usar_Plantas(tintura,'En envase con corcho poner la planta triturada y se agrega alcohol de caña y agua destilada, reposar en oscuridad una o dos semanas').
+como_Usar_Plantas(jugo,'exprimir la planta en un trapo limpio').
+como_Usar_Plantas(horchata,'se muelen las semillas agregando agua para formar una masilla, se cuela, se endulza y agrega agua para tomar').
+
+
+
+se_usa_para(abrojo,'diuretico').
+se_usa_para(abrojo,'infeccion de pecho').
+se_usa_para(abrojo,'inflamacion de los ojos').
+se_usa_para(abrojo,'inflamacion del higado').
+se_usa_para(abrojo,'favorecer la circulacion de la sangre').
+
+se_usa_para(acacia,'aliviar la garganta').
+se_usa_para(acacia,'quemaduras').
+se_usa_para(acacia,'tos').
+se_usa_para(acacia,'bronquitis').
+se_usa_para(acacia,'evita la formacion de ampollas').
+
+se_usa_para(acanto,'desinflama picaduras').
+se_usa_para(acanto,'antivenenoso').
+se_usa_para(acanto,'almorranas').
+se_usa_para(acanto,'ardores de orina').
+se_usa_para(acanto,'heridas por golpe o caida').
+se_usa_para(acanto,'dispepsia').
+se_usa_para(acanto,'disenteria').
+
+se_usa_para(aceitilla,'tonico nervioso').
+se_usa_para(aceitilla,'tonico cerebral').
+se_usa_para(aceitilla,'cansancio intelectual').
+se_usa_para(aceitilla,'depresion').
+se_usa_para(aceitilla,'mortificacion').
+se_usa_para(aceitilla,'pesares').
+se_usa_para(aceitilla,'histeria').
+
+se_usa_para(achicora,'diuretico').
+se_usa_para(achicora,'depurativo').
+se_usa_para(achicora,'digestion dificil').
+se_usa_para(achicora,'colicos biliosos').
+se_usa_para(achicora,'estreñimiento').
+se_usa_para(achicora,'reduccion de bilis').
+se_usa_para(achicora,'purificacion de los riñones').
+
+se_usa_para(briona_blanca,'expulsar parasitos').
+se_usa_para(briona_blanca,'diuretico').
+se_usa_para(briona_blanca,'vino digestivo').
+
+se_usa_para(canela,'condimento').
+se_usa_para(canela,'anemia').
+se_usa_para(canela,'digestion').
+se_usa_para(canela,'gases').
+se_usa_para(canela,'gripe').
+se_usa_para(canela,'subir la presion').
+
+se_usa_para(cedron,'colicos').
+se_usa_para(cedron,'nauseas').
+se_usa_para(cedron,'flatulencias').
+se_usa_para(cedron,'mala digestion').
+se_usa_para(cedron,'tonico nervioso').
+
+se_usa_para(cardo_santo,'limpiar los ojos').
+
+se_usa_para(cempasuchil,'expulsar parasitos intestinales').
+se_usa_para(cempasuchil,'dolores de estomago').
+se_usa_para(cempasuchil,'reblandece los tejidos en casos de absesos y tumores').
+se_usa_para(cempasuchil,'tonica para todo el organismo').
+
+
+
+se_prepara(abrojo,'se coce la raiz y se usa como agua de uso').
+se_prepara(acacia,'se arranca la sabia del arbol y se chupa').
+se_prepara(acacia,'se mezcla con clara de huevo formando un cataplasma').
+
+se_prepara(acanto,'aplicar directamente').
+se_prepara(acanto,'te').
+
+se_prepara(aceitilla,'te').
+se_prepara(aceitilla,'tonico').
+
+se_prepara(achicoria,'te').
+
+se_prepara(briona_blanca,'se utiliza la raiz molida').
+se_prepara(briona_blanca,'te').
+se_prepara(briona_blanca,'vino').
+
+se_prepara(canela,'te').
+se_prepara(canela,'te con limon y ron').
+
+se_prepara(cedron,'hojas frescas o secadas a la sombra').
+se_prepara(cedron,'cocimiento y agua de uso').
+
+se_prepara(cardo_santo,'se machaca la raiz en agua').
+se_prepara(cardo_santo,'se cocina la hoja').
+se_prepara(cardo_santo,'sacar jugo a la hoja').
+
+se_prepara(cempasuchil,'te de hoja de cempasuchil').
+
+
+
+uso(abroj2
+uso(cardo_santo,maceracion).
+uso(cardo_santo,jugo).
+uso(cempasuchil,infusion).
+
+
+
+efecto_planta(afrodisiaca,'exita el apetito sexual').
+efecto_planta(analgesica,'quita o modera el dolor').
+efecto_planta(anestesica,'insensibiliza el cuerpo').
+efecto_planta(antidiarreica,'controla diarreas').
+efecto_planta(antiespasmodica,'controla espasmos nerviosos').
+efecto_planta(antiflogistica,'actua contra inflamaciones').
+efecto_planta(antipiretica,'quita la fiebre').
+efecto_planta(antiseptica,'mata los tejidos').
+efecto_planta(aperitiva,'produce apetito').
+efecto_planta(astringente,'hace contraer los tejidos').
+efecto_planta(carminativa,'evita la formación de gases o provoca su expulsion').
+efecto_planta(colagoga,'ayuda a expulsar la bilis').
+efecto_planta(depurativa,'limpia y purifica la sangre').
+efecto_planta(diaforetica,'provoca sudar').
+efecto_planta(digestiva,'favorece la digestion').
+efecto_planta(diuretica,'provoca la orina').
+efecto_planta(emetica,'provoca nauseas y vomitos').
+efecto_planta(emenagoga,'activa la menstruacion').
+efecto_planta(estupefciente,'aquieta, duerme o atonta').
+efecto_planta(expectorante,'expulsa las flemas').
+efecto_planta(hemostatica,'corta o detiene hemorragias').
+efecto_planta(hepatica,'ayuda al higado').
+efecto_planta(laxante,'purga sin provocar diarrea').
+efecto_planta(pectoral,'ayuda al pecho').
+efecto_planta(pedante,'calma dolores intestinales').
+efecto_planta(tonica,'da fuerza al organismo').
+efecto_planta(toxica,'es venenosa').
+efecto_planta(vermifuga,'expulsa gusanos intestinales').
+efecto_planta(vulneraria,'cura llagas y heridas').
+
+
+
+efecto(abrojo,diuretica).
+efecto(abrojo,depurativa).
+efecto(abrojo,pectoral).
+efecto(abrojo,antiflogistica).
+
+efecto(acacia,expectorante).
+efecto(acacia,antiseptica).
+
+efecto(acanto,anttiflogistica).
+efecto(acanto,depurativa).
+efecto(acanto,diuretica).
+efecto(acanto,vulneraria).
+efecto(acanto,antidiarreica).
+efecto(acanto,digestiva).
+
+efecto(aceitilla,estupefaciente).
+efecto(aceitilla,tonica).
+
+efecto(achicoria,diuretica).
+efecto(achicoria,depurativa).
+efecto(achicoria,laxante).
+efecto(achicoria,colagoga).
+
+efecto(briona_blanca,vermifuga).
+efecto(briona_blanca,diuretica).
+efecto(briona_blanca,digestiva).
+efecto(briona_blanca,toxica).
+
+efecto(canela,tonica).
+efecto(canela,diaforetica).
+efecto(canela,digestiva).
+efecto(canela,carminativa).
+efecto(canela,expectorante).
+
+efecto(cedron,pedante).
+efecto(cedron,carminativa).
+efecto(cedron,digestiva).
+efecto(cedron,tonica).
+
+efecto(cardo_santo,toxica).
+
+efecto(cempasuchil,vermifuga).
+efecto(cempasuchil,pedante).
+efecto(cempasuchil,antiseptica).
+efecto(cempasuchil,tonica).
+
+
+
+resumen(X,Y,Z):-cientifico(X,Y),se_usa_para(X,Z).
+modo_uso(X,Z,W,Y):-uso(X,Y),como_Usar_Plantas(Y,Z),se_prepara(X,W).
+efectoPlanta(Z,X):-efecto(X,Y),efecto_planta(Y,Z).
+plantas_mismo_efecto(X, Z):- efecto(Z, X).
